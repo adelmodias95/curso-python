73f427f1df9e77deee70f1892e8c140ed5ea75eb
Adelmo Dias <adelmo.dias@mzgroup.com>
2025-11-12 18:24:42 -0300

    Aulas sobre variáveis

---
+++ modulo_1/tipos_variaveis/numeros.py
@@ -0,0 +1,5 @@
+# Inteiro
+numero_inteiro = 10
+
+# Real com ponto flutuante
+numero_real = 3.14
---
+++ modulo_1/variaveis.py
@@ -0,0 +1,7 @@
+# snake_case
+nome_completo = "Adelmo Dias"
+idade_usuario = 30
+
+# camelCase
+nomeCompleto = "Adelmo Dias"
+idadeUsuario = 30
